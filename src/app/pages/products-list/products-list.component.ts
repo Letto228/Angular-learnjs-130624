793c7b695fd53807f34bea4f06d70c14638083a8
Letto228 <learnjs@macbook-pro-egor.local>
2024-06-27 23:29:11 +0300

    feat: add @Input() @Output()

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,18 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
+import {Product} from '../../shared/products/product.interface';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly product = productsMock;
+
+    onProductBuy(productID: Product['_id']) {
+        // eslint-disable-next-line no-console
+        console.log('Buy product id', productID);
+    }
+}
