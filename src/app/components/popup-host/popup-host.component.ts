acd04c81c6e542fe8882b7150fa2965e2501dbe6
Kristina <patrakovakris@gmail.com>
2024-07-07 01:16:26 +0300

    feat: add popup-host

---
+++ src/app/components/popup-host/popup-host.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
+
+@Component({
+    selector: 'app-popup-host',
+    templateUrl: './popup-host.component.html',
+    styleUrls: ['./popup-host.component.css'],
+})
+export class PopupHostComponent {
+    @ViewChild('viewport', {read: ViewContainerRef, static: true})
+    private readonly viewport: ViewContainerRef | undefined;
+
+    @Input() set template(templateRef: TemplateRef<unknown> | null) {
+        this.viewport?.clear();
+
+        if (templateRef) {
+            this.viewport?.createEmbeddedView(templateRef);
+        }
+    }
+
+    get isOpen(): boolean {
+        return !this.viewport;
+    }
+}
