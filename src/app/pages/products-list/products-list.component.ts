05e8dce61744b1696345f41aa2919c92b253f6a4
sstol <sergeystol123@gmail.com>
2024-07-08 20:04:13 +0300

    feat: Add directive ScrollWithLoading for infinty scrolling with uploading

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, inject} from '@angular/core';
 import {productsMock} from '../../shared/products/products.mock';
 import {Product} from '../../shared/products/product.interface';
+import {LoadDirection} from '../../shared/load-direction.enum';
 
 @Component({
     selector: 'app-products-list',
@@ -24,5 +25,32 @@ export class ProductsListComponent {
     onProductBuy(id: Product['_id']) {
         // eslint-disable-next-line no-console
         console.log(id);
+    }
+
+    onLoad(loadDirection: LoadDirection) {
+        // Just for test
+        if (this.products === null || this.products.length === 0) {
+            return;
+        }
+
+        const randomProducts = this.getRandomProducts(this.products, 7);
+
+        if (loadDirection === LoadDirection.UP) {
+            this.products.unshift(...randomProducts);
+        } else {
+            this.products.push(...randomProducts);
+        }
+
+        this.changeDetectorRef.markForCheck();
+    }
+
+    private getRandomProducts(products: Product[], amount: number): Product[] {
+        for (let i = products.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+
+            [products[i], products[j]] = [products[j], products[i]];
+        }
+
+        return products.slice(0, amount);
     }
 }
